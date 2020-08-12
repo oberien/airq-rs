dd5dcdb7cf716a15a95ef130b200858b4823b8d1
oberien <jaro.fietz@gmx.de>
2020-08-12 10:55:27 +0200

    Rocket with PgPool

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,8 +8,15 @@ async fn hello() -> &'static str {
     "uiae"
 }
 
+fn create_pool() -> Result<PgPool> {
+    PgPoolOptions::new()
+        .max_connections(8)
+        .connect_lazy(env!("DATABASE_URL"))
+}
+
 #[rocket::launch]
 async fn rocket() -> rocket::Rocket {
     rocket::ignite()
+        .manage(create_pool())
         .mount("/", rocket::routes![hello])
 }
