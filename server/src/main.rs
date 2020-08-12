166cd770838ca63c6f6a47031c0e9907b0865eba
oberien <jaro.fietz@gmx.de>
2020-08-12 10:54:56 +0200

    Rocket async hello world

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::{PgPoolOptions, PgPool};
+use sqlx::Result;
+use rocket::State;
+use airq;
+
+#[rocket::get("/")]
+async fn hello() -> &'static str {
+    "uiae"
+}
+
+#[rocket::launch]
+async fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", rocket::routes![hello])
 }
